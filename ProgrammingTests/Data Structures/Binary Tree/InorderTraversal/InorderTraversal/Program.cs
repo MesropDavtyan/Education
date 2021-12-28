2880f5e2604e876e50654e7cb8dfd0575b47afef
Mesrop Davtyan <mesrop.meda93@gmail.com>
2021-12-28 20:29:36 +0100

    Minimal change

--- ProgrammingTests/Data Structures/Binary Tree/InorderTraversal/InorderTraversal/Program.cs
+++ ProgrammingTests/Data Structures/Binary Tree/InorderTraversal/InorderTraversal/Program.cs
@@ -6,7 +6,10 @@ namespace InorderTraversal
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            TreeNode root = new TreeNode(0, new TreeNode(2, new TreeNode(1, new TreeNode(5), new TreeNode(1)), null), new TreeNode(1, new TreeNode(3, null, new TreeNode(6)), new TreeNode(-1, null, new TreeNode(8))));
+
+            Solution solutionl = new Solution();
+            Console.WriteLine(solutionl.InorderTraversal(root));
         }
     }
 }
