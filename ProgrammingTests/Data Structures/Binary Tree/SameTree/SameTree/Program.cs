f661939c904bfcd314ce8ec9db690a6c5555542a
Mesrop Davtyan <mesrop.meda93@gmail.com>
2022-01-06 00:22:24 +0100

    100. Same Tree

---
+++ ProgrammingTests/Data Structures/Binary Tree/SameTree/SameTree/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SameTree
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            TreeNode first = new TreeNode(0, new TreeNode(2, new TreeNode(1, new TreeNode(5), new TreeNode(1)), null), new TreeNode(1, new TreeNode(3, null, new TreeNode(6)), new TreeNode(-1, null, new TreeNode(8))));
+            TreeNode second = new TreeNode(1, new TreeNode(2, new TreeNode(1, new TreeNode(5), new TreeNode(1)), null), new TreeNode(1, new TreeNode(3, null, new TreeNode(6)), new TreeNode(-1, null, new TreeNode(8))));
+
+            Console.WriteLine(solution.IsSameTree(first, second));
+        }
+    }
+}
---
+++ ProgrammingTests/Data Structures/Binary Tree/SameTree/SameTree/Solution.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SameTree
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public class Solution
+    {
+        public bool IsSameTreeRecursive(TreeNode firstRoot, TreeNode secondRoot)
+        {
+            if (firstRoot == null && secondRoot == null)
+            {
+                return true;
+            }
+            if (firstRoot == null || secondRoot == null || firstRoot.val != secondRoot.val)
+            {
+                return false;
+            }
+
+            return IsSameTree(firstRoot.left, secondRoot.left) && IsSameTree(firstRoot.right, secondRoot.right);
+        }
+
+        public bool IsSameTree(TreeNode firstRoot, TreeNode secondRoot)
+        {
+            Queue<TreeNode> nodes = new Queue<TreeNode>();
+            nodes.Enqueue(firstRoot);
+            nodes.Enqueue(secondRoot);
+
+            while (nodes.Count != 0)
+            {
+                TreeNode first = nodes.Dequeue();
+                TreeNode second = nodes.Dequeue();
+
+                if (first == null && second == null)
+                {
+                    continue;
+                }
+                if (first == null || second == null || first.val != second.val)
+                {
+                    return false;
+                }
+
+                nodes.Enqueue(first.left);
+                nodes.Enqueue(second.left);
+                nodes.Enqueue(first.right);
+                nodes.Enqueue(second.right);
+            }
+
+            return true;
+        }
+    }
+}
