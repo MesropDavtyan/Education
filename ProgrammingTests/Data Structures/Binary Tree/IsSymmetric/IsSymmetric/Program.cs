d912c3dad5924c8035100f9649d898a7305e5584
Mesrop Davtyan <mesrop.meda93@gmail.com>
2022-01-06 22:14:47 +0100

    108. Convert Sorted Array to Binary Search Tree (recursive)

---
+++ ProgrammingTests/Data Structures/Binary Tree/IsSymmetric/IsSymmetric/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace IsSymmetric
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            TreeNode root = new TreeNode(1,
+                new TreeNode(2,
+                    new TreeNode(4), new TreeNode(3)),
+                new TreeNode(2,
+                    new TreeNode(3), new TreeNode(3)));
+
+            Console.WriteLine(solution.isSymmetric(root));
+        }
+    }
+}
---
+++ ProgrammingTests/Data Structures/Binary Tree/IsSymmetric/IsSymmetric/Solution.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace IsSymmetric
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public class Solution
+    {
+        public bool isSymmetric(TreeNode root)
+        {
+            if (root == null)
+            {
+                return true;
+            }
+
+            return isSymmetric(root.left, root.right);
+        }
+
+        public bool isSymmetric(TreeNode left, TreeNode right)
+        {
+            if (left == null || right == null)
+            {
+                return left == null;
+            }
+            if (left.val != right.val)
+            {
+                return false;
+            }
+
+            return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+        }
+    }
+}
