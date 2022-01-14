54c1d84cd86d3b3eab2a8981a39fbd78f835f2d0
Mesrop Davtyan <mesrop.meda93@gmail.com>
2022-01-14 20:53:25 +0100

    Adding Art of Unit Testing section

---
+++ ArtOfUnitTesting/ArtOfUnitTesting2ndEd.Samples/Chapter1.Console/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chapter1.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                SimpleParserTests.TestReturnsZeroWhenEmptyString();
+            }
+            catch (Exception e)
+            {
+                //exception
+                Console.WriteLine(e);
+            }
+        }
+
+    }
+}
