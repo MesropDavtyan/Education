90683f4bc62e1cd4522f2b970a0ea85dfb4e488b
Mesrop Davtyan <mesrop.meda93@gmail.com>
2023-12-04 19:14:55 +0100

    Day Four part one and part two

--- Advent of Code 2023/Program.cs
+++ Advent of Code 2023/Program.cs
@@ -2,5 +2,8 @@
 
 //DayOne.SumCalibrationValues2(Path.GetFullPath("../../../InputData/input_day_1.txt"));
 
-DayTwo.CubeConundrum(Path.GetFullPath("../../../InputData/input_day_2.txt"));
-DayTwo.CubeConundrumPartTwo(Path.GetFullPath("../../../InputData/input_day_2.txt"));
+//DayTwo.CubeConundrum(Path.GetFullPath("../../../InputData/input_day_2.txt"));
+//DayTwo.CubeConundrumPartTwo(Path.GetFullPath("../../../InputData/input_day_2.txt"));
+
+DayFour.Scratchcards(Path.GetFullPath("../../../InputData/input_day_4.txt"));
+DayFour.ScratchcardsPartTwo(Path.GetFullPath("../../../InputData/input_day_4.txt"));
