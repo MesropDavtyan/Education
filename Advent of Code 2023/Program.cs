93321040ec6841c6016770a829eb32dfa5069eb6
Mesrop Davtyan <mesrop.meda93@gmail.com>
2023-12-09 22:10:31 +0100

    Day six

--- Advent of Code 2023/Program.cs
+++ Advent of Code 2023/Program.cs
@@ -5,5 +5,8 @@
 //DayTwo.CubeConundrum(Path.GetFullPath("../../../InputData/input_day_2.txt"));
 //DayTwo.CubeConundrumPartTwo(Path.GetFullPath("../../../InputData/input_day_2.txt"));
 
-DayFour.Scratchcards(Path.GetFullPath("../../../InputData/input_day_4.txt"));
-DayFour.ScratchcardsPartTwo(Path.GetFullPath("../../../InputData/input_day_4.txt"));
+//DayFour.Scratchcards(Path.GetFullPath("../../../InputData/input_day_4.txt"));
+//DayFour.ScratchcardsPartTwo(Path.GetFullPath("../../../InputData/input_day_4.txt"));
+
+DaySix.WaitForIt(Path.GetFullPath("../../../InputData/input_day_6.txt"));
+DaySix.WaitForItPartTwo(Path.GetFullPath("../../../InputData/input_day_6.txt"));
